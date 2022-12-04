ea45609e2e5277e2c040049b8d9667fb5153fa09
sandrini-code <gastonrg9@gmail.com>
2022-12-03 21:45:34 -0300

    commit exitoso

--- blog/apps/post/models.py
+++ blog/apps/post/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     categoria=models.ForeignKey(Categoria, on_delete=models.CASCADE)
     publicado=models.BooleanField(default=True)
     fecha_creacion=models.DateField(auto_now_add=True)
-    # usuario= models.foreign_key(User, on_delete=models.CASCADE)
+    usuario= models.ForeignKey(User, on_delete=models.CASCADE)
     class Meta:
         verbose_name_plural='Posteos'
     def __str__(self):
